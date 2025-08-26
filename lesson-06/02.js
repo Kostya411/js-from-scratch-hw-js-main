83d6d84d70f767512e7c6b645f26ee3eb66097af
Kostya441 <kostya.voropaev1998@mail.ru>
2025-08-26 11:53:00 +0300

    lesson 5

--- lesson-06/02.js
+++ lesson-06/02.js
@@ -26,8 +26,9 @@ console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 
 const map = (array, callBack) => {
     let result = [];
-    for (let i in array) {
-        result.push(callBack(array[i]));
+    for (let index in array) {
+        const element = array[index];
+        result.push(callBack(element, index));
     }
     return result;
 }
