0b2f04324d0181e627bcec8887db122f28da6037
Kostya441 <kostya.voropaev1998@mail.ru>
2025-08-26 12:13:43 +0300

    lesson 5

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -22,17 +22,22 @@ const oddNumbers = filter(numbers, (element, index) => {
 
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
-
+const indexes = [];
 const filter = (array, callBack) => {
     let resultArray = [];
-    for (let index =0; index < array.length; index++) {
+    for (let index = 0; index < array.length; index++) {
         let element = array[index];
         if (callBack(element, index)){
             resultArray.push(element);
         }
     }
+    console.log(resultArray);
     return resultArray;
 }
+
+
+
+
 
 
 
