2ea626c91baf412a5c432ff96b343534699cae22
Kostya441 <kostya.voropaev1998@mail.ru>
2025-09-03 05:59:12 +0300

    lesson 7

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -13,7 +13,43 @@
 */
 
 const WEB_TECH_IMAGES = [
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
-]
+    'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
+    'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
+    'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
+];
+let index_image = 0;
+
+
+window.addEventListener('load', function () {
+    console.log('Вся страница полностью загружена.');
+    // Запуск анимаций или сложных визуальных скриптов
+    const image = document.getElementById("web-tech-image");
+    const button_next = document.getElementById("next-button");
+    const button_prev = document.getElementById("prev-button");
+
+
+    image.setAttribute('src', WEB_TECH_IMAGES[index_image]);
+
+    button_next.addEventListener('click', function () {
+        console.log('clicked next');
+        index_image = function_image_swap(index_image += 1);
+    });
+    button_prev.addEventListener('click', function () {
+        console.log('clicked prev');
+        index_image = function_image_swap(index_image -= 1);
+    });
+
+    function function_image_swap(index) {
+        if (index > WEB_TECH_IMAGES.length - 1) {
+            index = 0;
+        }
+        if (index < 0){
+            index = WEB_TECH_IMAGES.length - 1;
+        }
+        console.log(`index_image+${index}`);
+        image.setAttribute('src', WEB_TECH_IMAGES[index]);
+        return index;
+    }
+
+
+})
